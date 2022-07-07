98a4d9ebd722d2c38c34aa26ffa43587800324a2
isi-73 <108881767+isi-73@users.noreply.github.com>
2022-07-07 16:08:09 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import socket
+from dhooks import Webhook, Embed 
+
+hook = Webhook("Wenhook-URL")
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname) 
+
+print(f"{hostname} IP Addr:{local_ip}")
+embed = Embed(
+  description=f"IP: ``{local_ip}``", 
+  color=0xa9a9a9
+)
+embed.set_author(name=f"{hostname}") 
+
+hook.send(embed=embed)
